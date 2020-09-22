f370c3f52ed937a50c445f24a19670a98e60f951
Maksim Kolesnikov <maksim.kolesnikov@ivkhk.ee>
2020-09-22 18:47:02 +0300

    Task 4

--- src/startdev/StartDev.java
+++ src/startdev/StartDev.java
@@ -19,13 +19,19 @@ public class StartDev {
         task1.run();
         
         Task2 task2 = new Task2();
-        task2.run(); */
+        task2.run(); 
         
         Task3 task3 = new Task3();
-        task3.run(); 
+        task3.run(); */
     
-       /* Task4 task4 = new Task4();
-        task4.run();*/
+        Task4 task4 = new Task4();
+        task4.run();
+       
+       /* Task5 task5 = new Task5();
+        task5.run();*/
+       
+       /* Task6 task6 = new Task6();
+        task6.run();*/
         // TODO code application logic here
     }
     
--- src/startdev/Task4.java
+++ src/startdev/Task4.java
@@ -11,5 +11,14 @@ package startdev;
  */
 class Task4 {
     public void run(){
-    }
+    int[] array = new int[10];
+        int sum = 0;
+            for (int i = 0; i < array.length; i++) {
+            array[i] = (int) Math.round((Math.random() * 20) - 0);
+
+            System.out.printf("%-3d",array[i]);
+            sum=sum+array[i];
+}
+            System.out.println("\nСумма всех ЧЁТНЫХ рандомных чисел:\n" + sum);
+}
 }
