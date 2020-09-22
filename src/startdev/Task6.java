527deef897ecdc3bec07e891aa22be4fecb183a0
Maxim-Rider <57655690+Maxim-Rider@users.noreply.github.com>
2020-09-22 20:48:36 +0300

    Update Task6.java

--- src/startdev/Task6.java
+++ src/startdev/Task6.java
@@ -29,32 +29,3 @@ class Task6 {
 }
 
 
-/*package startdev;
-
-import java.util.Arrays;
-import java.util.Scanner;
-
- class Task6 {
-    public void run(){
-        int[][]jaggedArr = {{1, 2}, {3, 4, 5}, {6, 7, 8, 9}};
-        jaggedArr[0]= new int[2];
-        jaggedArr[1]= new int[3];
-        jaggedArr[2]= new int[4];
-        void() int[][] = nulljaggedArr;
-{
-            Scanner sc = new Scanner(System.in);
-            for (int[] jaggedArr2 : jaggedArr) {
-                for (int inner = 0; inner < jaggedArr2.length; inner++) {
-                    jaggedArr2[inner] = sc.nextInt();
-                    void printElements
-                            int[][]
-                                    nulljaggedArr
-                                    {
-                                        for (int[] jaggedArr1 : jaggedArr) {
-                                            System.out.println(Arrays.toString(jaggedArr1));
-                                        }
-                                    }               }
-            }
-}
-   }
-}*/
